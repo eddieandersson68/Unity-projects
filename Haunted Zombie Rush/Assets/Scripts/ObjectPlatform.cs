359daf1bb696b7d37a2dc6bb9b3c2267c9dd8401
eddieandersson68 <eddie.andersson68@gmail.com>
2017-01-30 13:34:56 +0100

    First Commit
    Added unity game

---
+++ Haunted Zombie Rush/Assets/Scripts/ObjectPlatform.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPlatform : MonoBehaviour {
+
+    [SerializeField] private float objectSpeed =1;
+    [SerializeField] private float resetPosition = -24.9f;
+    [SerializeField] private float startPosittion = 77.66f;
+    // Use this for initialization
+    void Start () {
+	}
+
+	// Update is called once per frame
+	protected virtual void Update () {
+        if (!GameManager.instance.GameOver ) {
+            transform.Translate(Vector3.left * (objectSpeed * Time.deltaTime));
+
+            if (transform.localPosition.x <= resetPosition) {
+                Vector3 newPos = new Vector3(startPosittion, transform.position.y, transform.position.z);
+                transform.position = newPos;
+            } 
+        
+        }
+    }
+}
---
+++ Haunted Zombie Rush/Assets/Scripts/Rock.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rock : ObjectPlatform {
+
+    [SerializeField] Vector3 topPosition;
+    [SerializeField] Vector3 bottomPosition;
+    [SerializeField] float Speed;
+
+
+
+    // Use this for initialization
+    void Start () {
+        StartCoroutine (Move(bottomPosition));	
+	}
+
+    protected override void Update () {
+        if (GameManager.instance.PlayerActive) {
+            base.Update ();
+        }
+
+    }
+
+    IEnumerator Move(Vector3 target) {
+        while (Mathf.Abs((target - transform.localPosition).y) > 0.20f) {
+            Vector3 direction = target.y == topPosition.y ? Vector3.up : Vector3.down;
+            transform.localPosition += direction * Time.deltaTime * Speed;
+
+            yield return null;
+
+        }
+        
+
+        yield return new WaitForSeconds(0.5f);
+
+        Vector3 newTarget = target.y == topPosition.y ? bottomPosition : topPosition;
+
+        StartCoroutine(Move(newTarget));
+
+    }
+}
